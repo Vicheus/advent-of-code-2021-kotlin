88a0eddde0135edb9e81162c1acc9a82a53d3223
Oleksandr Kononchuk <oleksandr.k@tracktica.com>
2021-12-06 17:33:25 +0200

    Day01: Solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,19 @@
+import kotlin.math.ceil
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.drop(1).foldIndexed(0) { i: Int, acc: Int, el: String -> acc + ceil((el.toDouble() / input[i].toDouble()) - 1.0).toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val windowedInput = input.windowed(size = 3, step = 1)
+        return windowedInput.drop(1).foldIndexed(0) { i: Int, acc: Int, el: List<String> ->
+            val first = windowedInput[i].sumOf { it.toInt() }.toDouble()
+            val second = el.sumOf { it.toInt() }.toDouble()
+            val increment = ceil((second / first) - 1).toInt()
+            acc + increment
+        }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
